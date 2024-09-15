60c018b036da1d2de9d00948fb8cca2279132843
dan <dan@DESKTOP-32OJC27>
2024-09-14 20:20:28 -0400

    new update file

--- lua/update.lua
+++ lua/update.lua
@@ -1,40 +1,20 @@
-local component = require("component")
-local internet = component.internet
-local filesystem = require("filesystem")
 local shell = require("shell")
+local computer = require("computer")
 
--- Define the GitHub URL for raw files (replace USERNAME/REPO and branch/file path)
-local baseUrl = "https://raw.githubusercontent.com/Dnovak109/lua/master/lua/"
+-- Set the working directory where the updates will be applied
+shell.setWorkingDirectory("/home/reactorSetup/")
 
--- List of files you want to pull from GitHub
-local files = {
-  "update.lua",
---  "script2.lua"
-}
+-- Remove old files
+--shell.execute("rm file.lua")
 
--- Function to download a file from GitHub
-local function downloadFile(file)
-  local url = baseUrl .. file
-  local response = internet.request(url)
+-- Update by downloading new files from your repository
+print("Updating")
+-- shell.execute("wget https://raw.githubusercontent.com/Dnovak109/lua/master/lua/")
+shell.execute("wget https://raw.githubusercontent.com/Dnovak109/lua/master/lua/update.lua")
 
-  -- Check if the response is valid
-  if not response then
-    print("Failed to fetch:", url)
-    return
-  end
+-- Return to the home directory
+shell.setWorkingDirectory("/home/")
+print("Rebooting")
 
-  -- Create or overwrite the file on OpenComputers
-  local f = io.open("/home/" .. file, "w")
-
-  for chunk in response do
-    f:write(chunk)
-  end
-
-  f:close()
-  print("Downloaded:", file)
-end
-
--- Download all files
-for _, file in ipairs(files) do
-  downloadFile(file)
-end
+-- Reboot the computer
+computer.shutdown(true)
