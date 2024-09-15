6ac53d10e895246e08bd0f22e9746f8d0f9b564b
dan <dan@DESKTOP-32OJC27>
2024-09-15 03:07:18 -0400

    added displayitems

--- lua/update.lua
+++ lua/update.lua
@@ -10,6 +10,7 @@ print("Updating")
    shell.execute("wget -f https://raw.githubusercontent.com/Dnovak109/lua/master/lua/update.lua")
    shell.execute("wget -f https://raw.githubusercontent.com/Dnovak109/lua/master/lua/Reactor.lua")
    shell.execute("wget -f https://raw.githubusercontent.com/Dnovak109/lua/master/lua/sandbox.lua")
+   shell.execute("wget -f https://raw.githubusercontent.com/Dnovak109/lua/master/lua/diaplayItems.lua")
 
 -- Return to the home directory
 shell.setWorkingDirectory("/home/")
